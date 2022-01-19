096905901fc86f0a08de4d583f6705c45306dc7e
Tahreak Robinson <trob0697@gmail.com>
2022-01-19 18:21:04 -0500

    dotenv implementation

--- back-end/src/config.ts
+++ back-end/src/config.ts
@@ -1,17 +1,19 @@
+require('dotenv').config()
+
 import { TypeOrmModuleOptions } from "@nestjs/typeorm"
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: "postgres",
-    host: "localhost",
-    port: 5000,
-    username: "tahreak",
-    password: "",
-    database: "PokerTools",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_UN,
+    password: process.env.DB_PW,
+    database: process.env.DB_DB,
     entities: [__dirname + "src/**/*.entity{.ts,.js}"],
     synchronize: true,
     autoLoadEntities: true
 };
 
 export const jwtConstants = {
-    secret: "secretKey",
+    secret: process.env.JWT_SECRET,
 };
