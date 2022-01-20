46004d242124d3c9d764017f3654c42e4fd387d4
Tahreak Robinson <trob0697@gmail.com>
2022-01-20 13:57:51 -0500

    formating and fixes

--- src/config.ts
+++ src/config.ts
@@ -4,16 +4,14 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm"
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: "postgres",
     url: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    ssl: { rejectUnauthorized: false },
     // host: process.env.DB_HOST,
     // port: Number(process.env.DB_PORT),
     // username: process.env.DB_UN,
     // password: process.env.DB_PW,
     // database: process.env.DB_DB,
-    entities: ["dist/**/*.entity{.ts,.js}"],
-    synchronize: false,
+    entities: [__dirname + "src/**/*.entity{.ts,.js}"],
+    synchronize: (process.env.NODE_ENV === "dev" ? true : false),
     autoLoadEntities: true
 };
 
