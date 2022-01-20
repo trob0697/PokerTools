6fa3b72509b3a555b3c6472ed3a56442372c370e
Tahreak Robinson <trob0697@gmail.com>
2022-01-20 14:46:49 -0500

    merge master

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,10 @@
-require("dotenv").config()
-import { TypeOrmModuleOptions } from "@nestjs/typeorm"
+require("dotenv").config();
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: "postgres",
     url: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
-    // host: process.env.DB_HOST,
-    // port: Number(process.env.DB_PORT),
-    // username: process.env.DB_UN,
-    // password: process.env.DB_PW,
-    // database: process.env.DB_DB,
     entities: [__dirname + "src/**/*.entity{.ts,.js}"],
     synchronize: (process.env.NODE_ENV === "dev" ? true : false),
     autoLoadEntities: true
